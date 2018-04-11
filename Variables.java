698872ce8fda66b9a08ff72906f2d8ade40c34ba
EvgeniyDokhlenko <evdohlenko@yandex.ru>
2018-04-12 00:16:52 +0400

    Урок 2

---
+++ Variables.java
@@ -0,0 +1,12 @@
+public class Variables{
+	int id=11;
+	String name = " Сидоров Иван иванович ";
+	int age = 50;
+	public static void main(String[] args){
+		Variables variables = new Variables();
+		variables.person();
+	}
+	void person(){
+		System.out.println(id+name+age+" лет");
+	}
+}

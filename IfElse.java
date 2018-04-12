42408049204e685e6c638587d07716a36f8821ed
EvgeniyDokhlenko <evdohlenko@yandex.ru>
2018-04-12 23:36:11 +0400

    Версия от 12.04.2018

---
+++ IfElse.java
@@ -0,0 +1,16 @@
+public class IfElse {
+
+	public static void main(String[] args) {
+		int age = 12;
+	
+		if(age<10){
+			System.out.println("Child");
+		}
+		if(age>10 && age<20){
+			System.out.println("Teen");
+		}
+		else{
+			System.out.println("Adult");
+		}
+	}
+}
--- Variables.java
+++ Variables.java
@@ -2,11 +2,29 @@ public class Variables{
 	int id=11;
 	String name = " Сидоров Иван Вованович ";
 	int age = 50;
+	int i = 10;
+	int j = 15;
+	int k = i + j;
+	int x = k * 3;
+	int y = x % 6;
+	double z = 0.1;
+	double w = 0.2;
+	double r = z*w;
+	char c = 'A';
+	char a = 'B';
+
 	public static void main(String[] args){
 		Variables variables = new Variables();
 		variables.person();
+		variables.compilation();
 	}
 	void person(){
 		System.out.println(id+name+age+" лет");
+	}
+	void compilation(){
+		System.out.println("Результаты арифметических операций с типом int "+k+x+y);
+		System.out.println("Результаты арифметических операций с типом double r ="+r);
+		System.out.print("Результат сложения типов char  ");
+		System.out.println (c - a);
 	}
 }

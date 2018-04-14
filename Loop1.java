877f3d4208b2ac71837766b7ce410767cabc13c7
EvgeniyDokhlenko <evdohlenko@yandex.ru>
2018-04-14 18:47:02 +0400

    Обновление

---
+++ Loop1.java
@@ -0,0 +1,29 @@
+public class Loop1{
+	
+	public static void main(String args[]){
+		int x=2, y=5,z=20,q=25;
+		//Задали значения переменных
+		System.out.println("Задано x=2, y=5;");
+		if(x==1) System.out.println("Циклы и условия");
+		else System.out.println("Не выполнено условие x==1");
+		if (x<y){
+			System.out.println("Выполнено условие x<y");
+		}
+		while(y<z){
+		System.out.println(z+"  z - болше 5");
+		z=z-1;	
+		}
+		do{
+			x=x+1;
+			System.out.println(x+"   пока x меньше q, q=25");
+			
+		}while(x<q);
+		for(int count = 10; count > 0; count--){
+			System.out.println(count + "  count - болше 0");
+					for(int count1 = 0; count1 < 10; count1++){
+					System.out.println(count1 + "     count - меньше 10");
+					}
+		}		
+	}
+}
+
